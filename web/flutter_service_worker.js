6f4ee3632febab8f3a2ac1e18b64b4d9ac5caa1e
Jinwoo <njw9108@instapay.kr>
2022-05-31 11:09:05 +0900

    api test

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -5,9 +5,9 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "aba8a33fc6a90bb124298e8f7218bd90",
 "favicon.ico": "d8f5cf2830b85693d3f69d645f32e0b2",
-"index.html": "511f728c814b4b847988bf00fbeda622",
-"/": "511f728c814b4b847988bf00fbeda622",
-"main.dart.js": "e59e49878ab9a15bbf68118d30d6c169",
+"index.html": "0c3025599c3f076bf02c4caa23b80246",
+"/": "0c3025599c3f076bf02c4caa23b80246",
+"main.dart.js": "2f1e816b8aa9a0b85fefef470ccf1723",
 "flutter.js": "0816e65a103ba8ba51b174eeeeb2cb67",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
